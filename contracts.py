0733a42fd42199a74e6f12b358e7fa4d85076dd5
AB-code-crypto <alex-god@mail.ru>
2026-05-01 14:28:09 +0300

    Refactor FUT-only config and remove obsolete history comments

--- contracts.py
+++ contracts.py
@@ -63,34 +63,4 @@ Instrument: Registry = {
              "active_from_utc": "2026-09-16T22:00:00Z", "active_to_utc": "2026-12-16T22:00:00Z"},
         ]
     },
-    # "NQ": {
-    #     **FUT_DEFAULTS,
-    #     "tradingClass": "NQ",
-    #     "multiplier": 20.0,
-    #     "contracts": [
-    #         {"conId": 666754605, "localSymbol": "NQH5", "lastTradeDateOrContractMonth": "20250321",
-    #          "active_from_utc": "2025-01-01T00:00:00Z", "active_to_utc": "2025-03-19T22:00:00Z"},
-    #
-    #         {"conId": 672387474, "localSymbol": "NQM5", "lastTradeDateOrContractMonth": "20250620",
-    #          "active_from_utc": "2025-03-19T22:00:00Z", "active_to_utc": "2025-06-18T22:00:00Z"},
-    #
-    #         {"conId": 691171690, "localSymbol": "NQU5", "lastTradeDateOrContractMonth": "20250919",
-    #          "active_from_utc": "2025-06-18T22:00:00Z", "active_to_utc": "2025-09-17T22:00:00Z"},
-    #
-    #         {"conId": 563947738, "localSymbol": "NQZ5", "lastTradeDateOrContractMonth": "20251219",
-    #          "active_from_utc": "2025-09-17T22:00:00Z", "active_to_utc": "2025-12-17T23:00:00Z"},
-    #
-    #         {"conId": 730283097, "localSymbol": "NQH6", "lastTradeDateOrContractMonth": "20260320",
-    #          "active_from_utc": "2025-12-17T23:00:00Z", "active_to_utc": "2026-03-18T22:00:00Z"},
-    #
-    #         {"conId": 750150196, "localSymbol": "NQM6", "lastTradeDateOrContractMonth": "20260618",
-    #          "active_from_utc": "2026-03-18T22:00:00Z", "active_to_utc": "2026-06-16T22:00:00Z"},
-    #
-    #         {"conId": 770561204, "localSymbol": "NQU6", "lastTradeDateOrContractMonth": "20260918",
-    #          "active_from_utc": "2026-06-16T22:00:00Z", "active_to_utc": "2026-09-16T22:00:00Z"},
-    #
-    #         {"conId": 563947726, "localSymbol": "NQZ6", "lastTradeDateOrContractMonth": "20261218",
-    #          "active_from_utc": "2026-09-16T22:00:00Z", "active_to_utc": "2026-12-16T23:00:00Z"},
-    #     ],
-    # },
 }
