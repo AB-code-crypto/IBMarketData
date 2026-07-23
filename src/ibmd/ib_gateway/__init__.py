2c5cad2bf8d308cd16e2ac31ab86e77860b980dd
AB-code-crypto <alex-god@mail.ru>
2026-07-23 15:50:02 +0300

    feat: export reusable market-data gateway

--- src/ibmd/ib_gateway/__init__.py
+++ src/ibmd/ib_gateway/__init__.py
@@ -1,6 +1,12 @@
 """Reusable Interactive Brokers gateway contracts and test doubles."""
 
+from .fake_market_data import ScriptedMarketDataReader
 from .fake_positions import ScriptedPositionReader
+from .market_data import (
+    BrokerMarketDataReadError,
+    IBMarketDataReader,
+    RealtimeQuoteSubscription,
+)
 from .positions import (
     BrokerPositionReadError,
     IBPositionReader,
@@ -8,8 +14,12 @@ from .positions import (
 )
 
 __all__ = [
+    "BrokerMarketDataReadError",
     "BrokerPositionReadError",
+    "IBMarketDataReader",
     "IBPositionReader",
     "RawBrokerPosition",
+    "RealtimeQuoteSubscription",
+    "ScriptedMarketDataReader",
     "ScriptedPositionReader",
 ]
