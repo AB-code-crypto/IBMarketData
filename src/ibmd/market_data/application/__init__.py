7cac452edac2f31c83ad1f50bbc916ce24a25fe5
AB-code-crypto <alex-god@mail.ru>
2026-07-23 17:34:54 +0300

    feat: export market-data backfill API

--- src/ibmd/market_data/application/__init__.py
+++ src/ibmd/market_data/application/__init__.py
@@ -1,11 +1,19 @@
 """Application orchestration for target market data."""
 
+from .backfill import (
+    HistoryBackfillResult,
+    backfill_contract_history,
+    iter_history_chunks,
+)
 from .config import MarketDataConfigError, MarketDataShadowConfig
 from .service import MarketDataServiceError, MarketDataShadowService
 
 __all__ = [
+    "HistoryBackfillResult",
     "MarketDataConfigError",
     "MarketDataServiceError",
     "MarketDataShadowConfig",
     "MarketDataShadowService",
+    "backfill_contract_history",
+    "iter_history_chunks",
 ]
