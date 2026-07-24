fba620af4360d3386250c6f47f2600d4ab41968c
AB-code-crypto <alex-god@mail.ru>
2026-07-24 20:35:41 +0300

    feat: export paper order gateway

--- src/ibmd/ib_gateway/__init__.py
+++ src/ibmd/ib_gateway/__init__.py
@@ -6,15 +6,29 @@ from .broker_reconciliation import (
 )
 from .fake_broker_reconciliation import ScriptedBrokerReconciliationReader
 from .fake_market_data import ScriptedMarketDataReader
+from .fake_paper_orders import ScriptedPaperOrderGateway
 from .fake_positions import ScriptedPositionReader
 from .ib_async_broker_reconciliation import (
     IBAsyncBrokerReconciliationReader,
     IBBrokerReconciliationConnectionSettings,
 )
+from .ib_async_paper_orders import (
+    IBAsyncPaperOrderGateway,
+    IBPaperOrderConnectionSettings,
+    build_paper_market_order,
+    build_paper_order_contract,
+)
 from .market_data import (
     BrokerMarketDataReadError,
     IBMarketDataReader,
     RealtimeQuoteSubscription,
+)
+from .paper_orders import (
+    BrokerOrderSubmitError,
+    PaperMarketOrderRequest,
+    PaperOrderGateway,
+    PaperOrderRoute,
+    PaperOrderSubmissionReceipt,
 )
 from .positions import (
     BrokerPositionReadError,
@@ -24,16 +38,26 @@ from .positions import (
 
 __all__ = [
     "BrokerMarketDataReadError",
+    "BrokerOrderSubmitError",
     "BrokerPositionReadError",
     "BrokerReconciliationReadError",
     "IBAsyncBrokerReconciliationReader",
+    "IBAsyncPaperOrderGateway",
     "IBBrokerReconciliationConnectionSettings",
     "IBBrokerReconciliationReader",
     "IBMarketDataReader",
+    "IBPaperOrderConnectionSettings",
     "IBPositionReader",
+    "PaperMarketOrderRequest",
+    "PaperOrderGateway",
+    "PaperOrderRoute",
+    "PaperOrderSubmissionReceipt",
     "RawBrokerPosition",
     "RealtimeQuoteSubscription",
     "ScriptedBrokerReconciliationReader",
     "ScriptedMarketDataReader",
+    "ScriptedPaperOrderGateway",
     "ScriptedPositionReader",
+    "build_paper_market_order",
+    "build_paper_order_contract",
 ]
