47189125f87808876c2019ec887ae4dbc06bf868
AB-code-crypto <alex-god@mail.ru>
2026-07-24 15:33:18 +0300

    feat: export position projection domain

--- src/ibmd/execution/domain/__init__.py
+++ src/ibmd/execution/domain/__init__.py
@@ -5,11 +5,27 @@ from .model import (
     ExecutionFoundationPolicyV1,
     admit_strategy_command,
 )
+from .position_projection import (
+    POSITION_PROJECTION_REASON_PREFIX,
+    PositionProjectionError,
+    PositionProjectionPolicyV1,
+    PositionProjectionResult,
+    RegisteredFuturesContractV1,
+    merge_position_projection_readiness,
+    project_strategy_position,
+)
 
 __all__ = [
     "ExecutionAdmission",
     "ExecutionDomainError",
     "ExecutionFoundationFixtureV1",
     "ExecutionFoundationPolicyV1",
+    "POSITION_PROJECTION_REASON_PREFIX",
+    "PositionProjectionError",
+    "PositionProjectionPolicyV1",
+    "PositionProjectionResult",
+    "RegisteredFuturesContractV1",
     "admit_strategy_command",
+    "merge_position_projection_readiness",
+    "project_strategy_position",
 ]
