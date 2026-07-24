2cd87bff1bee87ec9bcec9286c43edba8313d873
AB-code-crypto <alex-god@mail.ru>
2026-07-24 17:19:07 +0300

    feat: export read-only reconciliation service

--- src/ibmd/execution/application/__init__.py
+++ src/ibmd/execution/application/__init__.py
@@ -3,15 +3,31 @@ from .ports import (
     ExecutionRepository,
     ServiceHealthPublisher,
 )
+from .read_only_reconciliation import (
+    BrokerAttemptSource,
+    BrokerReconciliationRepository,
+    ReadOnlyBrokerReconciliationService,
+    ReadOnlyBrokerSnapshotSource,
+    ReadOnlyReconciliationRun,
+    ReconciledBrokerAttempt,
+    reconciliation_run_payload,
+)
 from .service import (
     ExecutionFoundationConfig,
     ExecutionFoundationService,
 )
 
 __all__ = [
+    "BrokerAttemptSource",
+    "BrokerReconciliationRepository",
     "DecisionCommandSource",
     "ExecutionFoundationConfig",
     "ExecutionFoundationService",
     "ExecutionRepository",
+    "ReadOnlyBrokerReconciliationService",
+    "ReadOnlyBrokerSnapshotSource",
+    "ReadOnlyReconciliationRun",
+    "ReconciledBrokerAttempt",
     "ServiceHealthPublisher",
+    "reconciliation_run_payload",
 ]
