f5bca2facdc49d4dacb744d3b06cf555e7649c17
AB-code-crypto <alex-god@mail.ru>
2026-07-24 15:33:46 +0300

    feat: expose execution position projection

--- src/ibmd/execution/__init__.py
+++ src/ibmd/execution/__init__.py
@@ -3,11 +3,18 @@ from .application import (
     ExecutionFoundationService,
 )
 from .domain import (
+    POSITION_PROJECTION_REASON_PREFIX,
     ExecutionAdmission,
     ExecutionDomainError,
     ExecutionFoundationFixtureV1,
     ExecutionFoundationPolicyV1,
+    PositionProjectionError,
+    PositionProjectionPolicyV1,
+    PositionProjectionResult,
+    RegisteredFuturesContractV1,
     admit_strategy_command,
+    merge_position_projection_readiness,
+    project_strategy_position,
 )
 
 __all__ = [
@@ -17,5 +24,12 @@ __all__ = [
     "ExecutionFoundationFixtureV1",
     "ExecutionFoundationPolicyV1",
     "ExecutionFoundationService",
+    "POSITION_PROJECTION_REASON_PREFIX",
+    "PositionProjectionError",
+    "PositionProjectionPolicyV1",
+    "PositionProjectionResult",
+    "RegisteredFuturesContractV1",
     "admit_strategy_command",
+    "merge_position_projection_readiness",
+    "project_strategy_position",
 ]
