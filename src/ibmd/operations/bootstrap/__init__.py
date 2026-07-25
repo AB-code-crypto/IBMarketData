77c307dc0799ad125ec0e3620b122d6227760e92
AB-code-crypto <alex-god@mail.ru>
2026-07-26 00:48:09 +0300

    Add target bootstrap package

---
+++ src/ibmd/operations/bootstrap/__init__.py
@@ -0,0 +1,17 @@
+from .target import (
+    BootstrapStoreResultV1,
+    TargetBootstrapError,
+    TargetBootstrapManifestV1,
+    TargetBootstrapResultV1,
+    TargetDeploymentBootstrapper,
+    load_target_bootstrap_manifest,
+)
+
+__all__ = [
+    "BootstrapStoreResultV1",
+    "TargetBootstrapError",
+    "TargetBootstrapManifestV1",
+    "TargetBootstrapResultV1",
+    "TargetDeploymentBootstrapper",
+    "load_target_bootstrap_manifest",
+]
